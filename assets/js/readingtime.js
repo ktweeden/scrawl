bd0bb74a8369ab249e8d20550ce0127b7018efdb
Kate Preston <ktweeden@googlemail.com>
2016-04-13 15:00:59 +0800

    Completed first pass over mobile front page

--- assets/js/readingtime.js
+++ assets/js/readingtime.js
@@ -2,16 +2,13 @@
 // Returns estimated reading time for each article in index, based on 200wpm
 
 function readingTime (bodyString) {
-  console.log(bodyString);
   words = $.trim(bodyString).split(" ").length;
-  console.log(words);
   mins = Math.floor(words/200);
   if (mins < 1){mins = 1;};
   return  "<h1>" + mins + "</h1>" + "<div> min read</div>";
 }
 
 function populateReadingTime () {
-  console.log("populateReadingTime has been called")
   $(".content-string").each(function(){
     time = readingTime($(this).text());
     $(this).parent().append(time);
