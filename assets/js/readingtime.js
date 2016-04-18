cb64836786ede12671f7a9aa7a3708901ce6d30d
Kate Preston <ktweeden@googlemail.com>
2016-04-18 18:57:57 +0800

    updated readingtime.js so that the variables are no longer global

--- assets/js/readingtime.js
+++ assets/js/readingtime.js
@@ -2,15 +2,15 @@
 // Returns estimated reading time for each article in index, based on 200wpm
 
 function readingTime (bodyString) {
-  words = $.trim(bodyString).split(" ").length;
-  mins = Math.floor(words/200);
+  var words = $.trim(bodyString).split(" ").length;
+  var mins = Math.floor(words/200);
   if (mins < 1){mins = 1;};
   return  "<h1>" + mins + "</h1>" + "<div> min read</div>";
 }
 
 function populateReadingTime () {
   $(".content-string").each(function(){
-    time = readingTime($(this).text());
+    var time = readingTime($(this).text());
     $(this).parent().append(time);
   });
 }
